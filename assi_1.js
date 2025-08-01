5d77e12f44f166c1abf8637ccbb7a39623ee5468
vivek prajapati <vivek011prajapati@gmail.com>
2025-08-01 16:03:24 +0530

    set about, contact, html routes

--- assi_1.js
+++ assi_1.js
@@ -5,8 +5,15 @@ const port = 3000
 app.get("/", (req,res) => {
     res.send("Welcome to My API")
 })
-
-
+app.get("/about", (req, res)=>{ 
+   res.send("This is an About Page")
+})
+app.get("/contact", (req,res) =>{
+    res.json({ name: 'Vivek', email: 'youremail@example.com' })
+})
+app.get("/html", (req,res) => {
+   res.send("<h1>Hi, I am Vivek</h1>")
+})
 app.listen(port, () => {
    console.log("server is started")
 })
