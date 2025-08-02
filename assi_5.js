0c67778ab78b09782c6863c2f5ac76260c872b15
vivek prajapati <vivek011prajapati@gmail.com>
2025-08-02 07:54:25 +0530

    set middle ware

--- assi_5.js
+++ assi_5.js
@@ -1,6 +1,25 @@
 import express from "express"
 const app = express()
 const port =3000
+
+function middleware1(req, res, next) {
+  console.log('Middleware 1');
+  next();
+}
+
+function middleware2(req, res, next) {
+  console.log('Middleware 2');
+  next();
+}
+
+function middleware3(req, res, next) {
+  console.log('Middleware 3');
+  next();
+}
+
+app.get("/profile", middleware1, middleware2, middleware3, (req,res) => {
+    res.send("this is your profile")
+})
 app.listen(port,()=> {
     console.log(`Server is running on http://localhost:${port}`)
 })
