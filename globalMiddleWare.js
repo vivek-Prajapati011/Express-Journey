48a1b2dafc90eee938d0d5652a52f3657a191518
vivek prajapati <vivek011prajapati@gmail.com>
2025-08-03 18:20:45 +0530

    set admin route

--- globalMiddleWare.js
+++ globalMiddleWare.js
@@ -7,6 +7,10 @@ app.get("/",(req, res) => {
     res.send("this is home page")
 })
 
+app.get("/admin", (req, res) => {
+    res.send("this is admin page")
+})
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
 
