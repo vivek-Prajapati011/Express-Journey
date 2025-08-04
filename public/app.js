d1dc717b6fca6cafd084ede7067d585d000ee556
vivek prajapati <vivek011prajapati@gmail.com>
2025-08-04 22:36:04 +0530

    set express.static method

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 import express from express
 const app = express()
 
+app.use(express.static("public"))
 const port = 3000
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
